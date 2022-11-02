cd4337af452c4fc1b63783fe2f5edbaae33c29f0
Denis <den.scherbinin2018@yandex.ru>
2022-11-02 23:09:02 +0500

    fixed bug

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -9,25 +9,38 @@ def main():
     print(f'Hello, {name}!\nFind the greatest common divisor of given numbers.')
     while index < 3:
         num1 = random.randint(1, 1000)
+        print(f'Первое число будет - {num1}')
         num2 = random.randint(1, 1000)
+        print(f'Второе число будет - {num2}')
         list_num = [num1, num2]
+        print(f'Получилось два числа: {list_num}')
         list_num.sort(reverse=True)
-        remains = []
-        remains.append(list_num[0] % list_num[1])
-        remains.append(list_num[1] % remains[0])
-        i = 0
-        j = 1
-        while (remains[i] % remains[j]) != 0:
-            remains.append(remains[i] % remains[j])
-            i = i + 1
-            j = j + 1
-
+        print(f'Отсортированые: {list_num}')
         print(f'Question: {num1} {num2}')
         answer = prompt.string('Your answer: ')
-        if int(answer) == remains[-1]:
+        if list_num[0] % list_num[1] == 0 and list_num[1] % list_num[1] == 0:
+            result = list_num[1]
+        else:
+            remains = []
+            remains.append(list_num[0] % list_num[1])
+            remains.append(list_num[1] % remains[0])
+            print(f'Остатки {remains}')
+            i = 0
+            j = 1
+            while (remains[i] % remains[j]) != 0:
+                if remains[j] == 0:
+                    result = remains[i]
+                    break
+                remains.append(remains[i] % remains[j])
+                print(f'Список делителей - {remains}')
+                print(f'Сейчас будем делить {remains[i]} на {remains[j]}')
+                result = remains[-1]
+                i = i + 1
+                j = j + 1
+        if result == int(answer):
             print('Correct!')
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{remains[-1]}'\nLet's try again, {name}!")
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'\nLet's try again, {name}!")
             break
         index = index + 1
     if index == 3:
